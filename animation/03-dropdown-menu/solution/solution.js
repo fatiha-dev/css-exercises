09df4be08043833d8e19cd3ecc42957176d34404
fatiha-dev <tihaelbou@gmail.com>
2023-07-30 22:53:54 +0100

    add transitions

--- animation/03-dropdown-menu/solution/solution.js
+++ animation/03-dropdown-menu/solution/solution.js
@@ -10,6 +10,8 @@ menuTitle.addEventListener("click", (e) => {
 
 window.addEventListener("click", (e) => {
   if (!dropdownContainer.contains(e.target)) {
-    dropdownMenu.classList.remove("visible")
+    dropdownMenu.classList.remove("visible");
+    
+
   }
 })
